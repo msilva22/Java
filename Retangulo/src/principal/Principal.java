888e86b26098c0b7b7b67a89b0017f8884a3f720
Moisés Silva <moises.ambiental@hotmail.com>
2023-03-28 19:35:04 -0300

    exercicio retangulo

---
+++ Retangulo/src/principal/Principal.java
@@ -0,0 +1,27 @@
+package principal;
+
+import programa.Rectangle;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Rectangle rect = new Rectangle();
+		System.out.println(" Enter rectangle Width and height");
+		rect.width = sc.nextDouble();	
+		rect.height = sc.nextDouble();
+		
+		System.out.printf("AREA = %.2f%n" , rect.area());
+		System.out.println();
+		System.out.printf("PERIMETER = %.2f%n" , rect.perimeter());
+		System.out.println();
+		System.out.printf("DIAGONAL = %.2f%n" , rect.diagonal());
+		sc.close();
+
+	}
+
+}
---
+++ Retangulo/src/programa/Rectangle.java
@@ -0,0 +1,20 @@
+package programa;
+
+public class Rectangle {
+	public double width;
+	public double height;
+	
+	public double area() {
+		return width * height;
+	}
+	
+	public double perimeter() {
+		return width + width + height +height;
+	}
+	
+	public double diagonal() {
+		return Math.sqrt(height*height + width *width );
+	}
+
+	
+}
