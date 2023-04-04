eac0db15df834f19f75c5b378d3aca057f9c551f
Moisés Silva <moises.ambiental@hotmail.com>
2023-04-04 13:59:44 -0300

    exercicio arquivos

---
+++ vetor1/arquivos/src/application/Program5.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a file path: ");
+		String strPath = sc.nextLine();
+		
+		
+		File path = new File(strPath);
+		
+		System.out.println("getName: " + path.getName());
+		
+		System.out.println("getParent: " + path.getParent());
+		
+		System.out.println("getPaht: " + path.getPath());
+		
+		
+		
+		sc.close();
+	}
+}
+
+
